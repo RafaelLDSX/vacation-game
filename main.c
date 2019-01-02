bf84939ef16bdda9bf4bfc148df772e7a06af56b
Rafael Xavier <rafael_ldsx@hotmail.com>
2019-01-02 13:09:56 -0200

    Drawing small red dot at the top of the screen
    -Corrected how windows were created
    -Created renderer to draw small red dot on screen

--- main.c
+++ main.c
@@ -8,12 +8,29 @@ int main(void)
 
 	//Creating a window
 	SDL_Window *main_window;
-	SDL_CreateWindow("MemGame",
+	main_window = SDL_CreateWindow("MemGame",
 					SDL_WINDOWPOS_UNDEFINED,
 					SDL_WINDOWPOS_UNDEFINED,
 					800,
 					600,
 					SDL_WINDOW_OPENGL );
+
+	//Creating renderer
+	SDL_Renderer *main_renderer;
+	main_renderer = SDL_CreateRenderer(main_window,
+					  -1,
+					  SDL_RENDERER_ACCELERATED);
+
+	SDL_RenderClear(main_renderer);
+
+	//Changing renderer color to draw points
+	SDL_SetRenderDrawColor(main_renderer, 255, 0, 0, 255);
+
+	//Actually drawing on screen
+	SDL_RenderDrawPoint(main_renderer, 1, 1);
+
+	//Updating screen
+	SDL_RenderPresent(main_renderer);
 
 	//Waiting 3 seconds
 	SDL_Delay(3000);
